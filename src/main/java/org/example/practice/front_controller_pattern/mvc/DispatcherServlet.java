5643dace81700c82eafbe710c83ff5e1f5ca2ae6
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-05 01:55:27 +0900

    feat: Implement DispatcherServlet using by front controller pattern

--- src/main/java/org/example/practice/front_controller_pattern/mvc/DispatcherServlet.java
+++ src/main/java/org/example/practice/front_controller_pattern/mvc/DispatcherServlet.java
@@ -1,8 +1,10 @@
 package org.example.practice.front_controller_pattern.mvc;
 
+import org.example.practice.front_controller_pattern.mvc.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,10 +15,29 @@ import java.io.IOException;
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
 	private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+	private RequestMappingHandlerMapping rmhm;
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public void init() throws ServletException {
+//		super.init();
+		rmhm = new RequestMappingHandlerMapping();
+		rmhm.init();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		super.service(req, resp);
-		log.info("DispatcherServlet#service");
+		log.info("[DispatcherServlet] service start");
+		try {
+			Controller handler = rmhm.findHandler(request.getRequestURI());
+			String viewName = handler.handleRequest(request, response);
+			
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+			requestDispatcher.forward(request, response);
+			
+		} catch (Exception e) {
+			log.error("exception occurred: {}", e.getMessage(), e);
+			throw new ServletException(e);
+		}
 	}
 }
