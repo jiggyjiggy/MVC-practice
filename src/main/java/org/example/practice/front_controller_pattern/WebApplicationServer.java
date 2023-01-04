08abe3defe7f5dad9de30ca024fece74a478fe9f
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-05 01:24:14 +0900

    feat: Set up tomcat

--- src/main/java/org/example/practice/front_controller_pattern/WebApplicationServer.java
+++ src/main/java/org/example/practice/front_controller_pattern/WebApplicationServer.java
@@ -1,7 +1,22 @@
 package org.example.practice.front_controller_pattern;
 
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 public class WebApplicationServer {
-	public static void main(String[] args) {
-	
+	private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+	public static void main(String[] args) throws Exception {
+		String webappDirLocation = "webapps/";
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		
+		tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+		log.info("configuring app with baseDir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+		
+		tomcat.start();
+		tomcat.getServer().await();
 	}
 }
