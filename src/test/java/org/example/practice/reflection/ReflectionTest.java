5693d801fd14060ac66f9e0b44a000788aaa5c91
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-04 23:31:44 +0900

    test: heap 영역에 load되어 있는 class type object를 가져오는 3가지 방법과 class의 내부에 접근하는 방법

--- src/test/java/org/example/practice/reflection/ReflectionTest.java
+++ src/test/java/org/example/practice/reflection/ReflectionTest.java
@@ -2,15 +2,21 @@ package org.example.practice.reflection;
 
 import org.example.practice.reflection.annotation.Controller;
 import org.example.practice.reflection.annotation.Service;
+import org.example.practice.reflection.model.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
 	private final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -48,5 +54,39 @@ public class ReflectionTest {
 		annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
 		
 		return beans;
+	}
+	
+	@DisplayName("class의 내부에 접근할 수 있다.")
+	@Test
+	void showInsideOfClass() {
+		Class<User> clazz = User.class;
+		
+		logger.debug(clazz.getName());
+		
+		logger.debug("User all declared fields: {}", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+		logger.debug("User all declared constructors: {}", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+		logger.debug("User all declared methods: {}", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+	}
+	
+	@DisplayName("heap 영역에 load되어 있는 class type object를 가져오는 3가지 방법이 있다")
+	@Test
+	void loadObjectOfClassTypeOnHeap() throws ClassNotFoundException {
+		// 첫번째 방법
+		Class<User> clazz = User.class;
+		
+		// 두번째 방법
+		User user = new User("jiggyjiggy", "기성");
+		Class<? extends User> clazz2 = user.getClass();
+		
+		// 세번째 방법
+		Class<?> clazz3 = Class.forName("org.example.practice.reflection.model.User");
+		
+		logger.debug("clazz: {}", clazz);
+		logger.debug("clazz2: {}", clazz2);
+		logger.debug("clazz3: {}", clazz3);
+		
+		assertThat(clazz == clazz2).isTrue();
+		assertThat(clazz2 == clazz3).isTrue();
+		assertThat(clazz3 == clazz).isTrue();
 	}
 }
