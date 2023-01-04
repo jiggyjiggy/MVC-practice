645bd1220e25ac3b2c3cb41660eae89919d8f153
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-04 23:12:08 +0900

    test: Add component scan

--- src/test/java/org/example/practice/reflection/ReflectionTest.java
+++ src/test/java/org/example/practice/reflection/ReflectionTest.java
@@ -7,29 +7,46 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ReflectionTest {
 	private final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 	
 	@Test
+	void componentScan() {
+		Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+		logger.debug("component, beans: {}", beans);
+	}
+	
+	@Test
 	void controllerScan() {
-		Reflections reflections = new Reflections("org.example.practice.reflection");
-		
-		Set<Class<?>> beans = new HashSet<>();
-		beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));  // controller들을 Bean에 등록
-		
-		logger.debug("beans: {}", beans);
+		Set<Class<?>> beans = getTypesAnnotatedWith(Controller.class);
+		logger.debug("controller, beans: {}", beans);
 	}
 	
 	@Test
 	void serviceScan() {
+		Set<Class<?>> beans = getTypesAnnotatedWith(Service.class);
+		logger.debug("service, beans: {}", beans);
+	}
+	
+	private static Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation> annotation) {
 		Reflections reflections = new Reflections("org.example.practice.reflection");
 		
 		Set<Class<?>> beans = new HashSet<>();
-		beans.addAll(reflections.getTypesAnnotatedWith(Service.class));  // service들을 Bean에 등록
+		beans.addAll(reflections.getTypesAnnotatedWith(annotation));
 		
-		logger.debug("beans: {}", beans);
+		return beans;
+	}
+	private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+		Reflections reflections = new Reflections("org.example.practice.reflection");
+		
+		Set<Class<?>> beans = new HashSet<>();
+		annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+		
+		return beans;
 	}
 }
