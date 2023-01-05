fdcd179ba8449788201dcfc3060c37ee6a87bae4
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-05 14:35:00 +0900

    feat: Add "GET /users"

--- src/main/java/org/example/actual/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/actual/mvc/RequestMappingHandlerMapping.java
@@ -2,6 +2,7 @@ package org.example.actual.mvc;
 
 import org.example.actual.mvc.controller.Controller;
 import org.example.actual.mvc.controller.HomeController;
+import org.example.actual.mvc.controller.UserListController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ public class RequestMappingHandlerMapping {
 	
 	void init() {
 		mappings.put("/", new HomeController());
+		mappings.put("/users", new UserListController());
 	}
 	
 	public Controller findHandler(String uriPath) {
