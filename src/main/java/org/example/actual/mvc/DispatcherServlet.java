a9ad996ee8887a38a4a415181cc0207adbca51e7
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-05 17:39:54 +0900

    feat: Implement ViewResolver

--- src/main/java/org/example/actual/mvc/DispatcherServlet.java
+++ src/main/java/org/example/actual/mvc/DispatcherServlet.java
@@ -1,6 +1,9 @@
 package org.example.actual.mvc;
 
 import org.example.actual.mvc.controller.Controller;
+import org.example.actual.mvc.view.JspViewResolver;
+import org.example.actual.mvc.view.View;
+import org.example.actual.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,17 +14,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
 	private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 	private RequestMappingHandlerMapping rmhm;
+	private List<ViewResolver> viewResolvers;
 	
 	@Override
 	public void init() throws ServletException {
 //		super.init();
 		rmhm = new RequestMappingHandlerMapping();
 		rmhm.init();
+		
+		viewResolvers = Collections.singletonList(new JspViewResolver());
 	}
 	
 	@Override
@@ -33,14 +42,12 @@ public class DispatcherServlet extends HttpServlet {
 			// [issue 2] spring web MVC framework에 비교했을때, handler에 대해 바로 실행하고있다
 			// Handler adapter를 통해야한다
 			
-			// [issue 1] redirect:/users <- 처리되고있지 않음
 			String viewName = handler.handleRequest(request, response);
-			// [issue 3] spring web MVC framework에 비교했을때, view에 대한 판단 과정이 없다
-			// View resolver를 통해서 redirect인지 forward인지 확인하는 과정이 필요하다
 			
-			// [issue 1] forward 만 처리한 코드
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-			requestDispatcher.forward(request, response);
+			for (ViewResolver viewResolver : viewResolvers) {
+				View view = viewResolver.resolveView(viewName);
+				view.render(new HashMap<>(), request, response);
+			}
 			
 		} catch (Exception e) {
 			log.error("exception occurred: {}", e.getMessage(), e);
---
+++ src/main/java/org/example/actual/mvc/view/ViewResolver.java
@@ -0,0 +1,5 @@
+package org.example.actual.mvc.view;
+
+public interface ViewResolver {
+	View resolveView(String viewName);
+}
