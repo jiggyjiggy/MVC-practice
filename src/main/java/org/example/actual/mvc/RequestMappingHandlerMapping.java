f9f093223dedb3e5b13b365581cfc2ac2562406d
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-05 16:15:10 +0900

    feat: Create forward controller

--- src/main/java/org/example/actual/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/actual/mvc/RequestMappingHandlerMapping.java
@@ -1,6 +1,7 @@
 package org.example.actual.mvc;
 
 import org.example.actual.mvc.controller.Controller;
+import org.example.actual.mvc.controller.ForwardController;
 import org.example.actual.mvc.controller.HomeController;
 import org.example.actual.mvc.controller.UserCreateController;
 import org.example.actual.mvc.controller.UserListController;
@@ -15,6 +16,7 @@ public class RequestMappingHandlerMapping {
 		mappings.put(new HandlerKey(RequestMethod.GET, "/"), new HomeController());
 		mappings.put(new HandlerKey(RequestMethod.GET, "/users"), new UserListController());
 		mappings.put(new HandlerKey(RequestMethod.POST, "/users"), new UserCreateController());
+		mappings.put(new HandlerKey(RequestMethod.GET, "/user/form"), new ForwardController("/user/form.jsp"));
 	}
 	
 	public Controller findHandler(HandlerKey handlerKey) {
